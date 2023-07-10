f81883d62c3db0c95a71b77dc1dffd93f1325128
Sanket Pawar <131710375+sanketpwr15@users.noreply.github.com>
2023-07-10 13:45:41 +0530

    Sendmail.py

--- Sendmail.py
+++ Sendmail.py
@@ -5,16 +5,21 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.base import MIMEBase
 from email import encoders
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
 
 # Email configuration
-sender_email = '@outlook.com'
-sender_password = ''
-receiver_email = '@gmail.com'
+sender_email = os.getenv("MU")
+sender_password = os.getenv("MP")
+receiver_email = os.getenv("RM")
+server_name = os.getenv('SERVER_NAME')
 subject = 'CSV File Attachment'
 body = 'Hello,\n\nPlease find the attached CSV file.\n\nRegards,\nTest Email'
 
 # Path to the CSV file
-csv_path = 'C:/Users/Pawar/Desktop/projects/Fetch_data_from_sql_and_export_it_into_csv_and_send_it_on_mail/output.csv'
+csv_path = 'C:/Users/user/Desktop/enctest/output.csv'
 
 # Create a multipart message object
 message = MIMEMultipart()
@@ -31,11 +36,12 @@ with open(csv_path, 'rb') as file:
     attachment = MIMEBase('application', 'octet-stream')
     attachment.set_payload(file.read())
     encoders.encode_base64(attachment)
-    attachment.add_header('Content-Disposition', f'attachment; filename="{os.path.basename(csv_path)}"')
+    attachment.add_header(
+        'Content-Disposition', f'attachment; filename="{os.path.basename(csv_path)}"')
     message.attach(attachment)
 
 # Connect to the SMTP server and send the email
-with smtplib.SMTP('smtp.office365.com', 587) as server:
+with smtplib.SMTP(server_name, 587) as server:
     server.starttls()
     server.login(sender_email, sender_password)
     server.send_message(message)
